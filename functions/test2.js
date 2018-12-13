41dbe71b4ed96a445a68f4493cb26892cc9d6fc5
Les Carbonaro <leslie.carbonaro@magna.com>
2018-12-13 15:12:02 -0500

    update

--- functions/test2.js
+++ functions/test2.js
@@ -3,19 +3,19 @@ const cheerio = require('cheerio');
 
 exports.handler = function (event, context, callback) {
 
-    const url = `https://www.indeed.ca/jobs?q=pharmacist&l=Milton&radius=40`;
+    const url = 'https://www.indeed.ca/jobs?q=pharmacist&l=Milton&radius=40';
 
     // get data from url
     // then send it
     const test = () => {
         axios.get(url)
-            .then(res => {
+            .then(async (res) => {
                 //console.log('dot then');
                 if (res.status === 200) {
                     const html = res.data;
                     const $ = cheerio.load(html);                    
 
-                    $('td#resultsCol').find('div.result').each(function (i, elem) {
+                    $('td#resultsCol').find('div.result').each(await function (i, elem) {
                         let jt = $(this).find('a.jobtitle').text().trim();
                         let jl = $(this).find('a.jobtitle').attr('href').trim();
 
