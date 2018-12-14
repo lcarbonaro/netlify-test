f0a4a767a51770024d71c9615b5b2008b1301b3b
Les Carbonaro <leslie.carbonaro@magna.com>
2018-12-14 15:15:22 -0500

    update

--- functions/test2.js
+++ functions/test2.js
@@ -16,16 +16,46 @@ exports.handler = function (event, context, callback) {
                     const $ = cheerio.load(html);                    
                     let result = [];
 
-                    $('div.result','td#resultsCol').each(await function (i, elem) {
+                    $('td#resultsCol div.result').each(await function (i, elem) {
                         let rec = {}
+                         
+                        //console.log( $(this).attr('id') );
+
                         let jts = $(this).find('a'); 
                         let jt = $(jts[0]).text().trim();
                         let jl = 'https://www.indeed.ca' + $(jts[0]).attr('href').trim();
 
-                        //console.log(`[${i + 1}]${jt}`);
+                        let co = $(this).find('span.company a').text().trim(); 
+                       
+
+                        if(co === '') {
+                            console.log('retry1');
+                            let co = $(this).find('div.sjcl > div:nth-child(1) > span').text().trim(); 
+                        }
+                        if(co === '') {
+                            console.log('retry2');
+                            let co = $(this).find('div > div:nth-child(1) > span > a').text().trim(); 
+                        }
+                        if(co === '') {
+                            console.log('retry3');
+                            let co = $(this).find('div > div:nth-child(1) > span').text().trim(); 
+                        }
+                        
+                        console.log(`*${co}*`);
+
+
+                        let lo = $(this).find('div.location').text().trim(); 
+
+                        if(lo === '') {
+                            let lo = $(this).find('span.location').text().trim(); 
+                        }
+
+                        //console.log(`[${i + 1}]${jt} co:${co} lo:${lo}`);
 
                         rec.jobTitle = jt;
                         rec.jobLink = jl;
+                        rec.company = co;
+                        rec.location = lo;
                         result.push(rec);
 
                     });
